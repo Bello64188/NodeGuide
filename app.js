9232513a0f577078552df94a60ebbed5cd3da1e2
abdulazeez <bello.m1605299@st.futminna.edu.ng>
2025-07-10 18:03:13 +0100

    update

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http= require('http')
 
-const express= require('express')
+const express= require('express');
 
 const app=  express();
 
@@ -11,7 +11,7 @@ app.use((req,res,next)=>{
 
 app.use((req,res,next)=>{
 console.log('Middleware two')
-res.end("Hello world")
+res.send(JSON.stringify({"name":"Bello Abdulazeez"}))
 });
 
 const server = http.createServer(app);
